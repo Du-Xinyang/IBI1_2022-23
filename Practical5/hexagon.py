ed2ccda9ca6143fb2000595a97cdcf457de4f779
Du Xinyang/Nancy <127186904+Du-Xinyang@users.noreply.github.com>
2023-04-24 22:25:18 +0800

    hexagon.py

---
+++ Practical5/hexagon.py
@@ -0,0 +1,8 @@
+#calculates the hexagon sequence and computes and displays the first five values
+#creat two variables, n and h
+for n in range (1,6):
+    h = n*(2*n - 1)
+#print 5 values
+    print (h)
+
+#1 6 15 28 45
