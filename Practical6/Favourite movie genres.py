4e095afbedda81fad89a9ea10d581a3451f22736
Du Xinyang/Nancy <127186904+Du-Xinyang@users.noreply.github.com>
2023-05-22 19:15:27 +0800

    Update Favourite movie genres.py

--- Practical6/Favourite movie genres.py
+++ Practical6/Favourite movie genres.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 movie = {'Comedy': 73, 'Action': 42, 'Romance': 38, 'Fantasy': 28, 'Science-fiction': 22, 'Horror': 19, 'Crime': 18, 'Documentary': 12, 'History': 8, 'War':7}
 # print the dictionary
 print(movie)
-# print the number of university students who prefer a movie genre taken from the input list.
-print("Comedy:", movie['Comedy'])
+# create a variable that can be modified and print the number of university students who prefer a movie genre taken from the input list.
+a = 'Comedy'
+print(a,":", movie[a])
 # construct a pie chart
 labels = 'Comedy', 'Action', 'Romance', 'Fantasy', 'Science-fiction', 'Horror', 'Crime', 'Documentary', 'History', 'War'
 sizes = [73, 42, 38, 28, 22, 19, 18, 12, 8, 7]
