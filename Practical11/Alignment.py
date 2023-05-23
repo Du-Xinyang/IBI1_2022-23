d6782c23dcd9c4a08c4f2f5e1d4641e383854ad8
Du Xinyang/Nancy <127186904+Du-Xinyang@users.noreply.github.com>
2023-05-23 11:13:19 +0800

    correct the answers

--- Practical11/Alignment.py
+++ Practical11/Alignment.py
@@ -1,63 +1,57 @@
-# the findings: 1.the alignment score for human and cat:119, the percentage:14.76%
-#               2.the alignment score for human and mouse:144, the percentage:17.87%
-#               3.the alignment score for cat and mouse：147， the percentage:18.24%
+# the findings: 1.the alignment score for human and cat:3717.0, the percentage:0.8521739130434782
+#               2.the alignment score for human and mouse:3579.0.0, the percentage:8211180124223603%
+#               3.the alignment score for cat and mouse：3592.0， the percentage:0.817391304347826%
 # the sequences of cat and mouse are most closely related
 # We can make biological or evolutionary interpretation from these data that cat and mouse have closer genetic relationship though cat is the natural enemy of mouse. And human has closer genetic relationship with mouse that cat.
 
-
-# compare the sequence between human and cat
+# use the blosum package to access blosum62 matrix
+# use itertools to combine three sequences
+import blosum as bl
+matrix = bl.BLOSUM(62)
+# create a dictionary
+dic = dict()
+#open the three files.
 # extract the sequence line
-file_human = open('ACE2_human.fa')
-file_cat = open('ACE2_cat.fa')
-output1 = open('human_gene','w')
-for line in file_human:
-    if not line.startswith('>'):
-        output1.write(line)
-        output1.close()
-file1 = open('human_gene')
-seq1 = file1.read()
-output2 = open('cat_gene','w')
-for line in file_cat:
-    if not line.startswith('>'):
-        output2.write(line)
-        output2.close()
-file2 = open('cat_gene')
-seq2 = file2.read()
-# compare the sequence
-edit_distance1 = 0
-for i in range(len(seq1)):
-    if seq1[i]!=seq2[i]:
-       edit_distance1 += 1
-print(edit_distance1)
-# calculate the	percentage	of identical amino acids for each of the three	comparisons
-percentage1 = edit_distance1/len(seq1)*100
-print("%.2f%%" % percentage1)
+with open('ACE2_human.fa', 'r') as f:
+    n = 0
+    for line in f:
+        n += 1
+        if n == 2:
+            dic['human'] = line[: -1]
+with open('ACE2_mouse.fa', 'r') as f:
+    n = 0
+    for line in f:
+        n += 1
+        if n == 2:
+            dic['mouse'] = line[: -1]
+with open('ACE2_cat.fa', 'r') as f:
+    n = 0
+    for line in f:
+        n += 1
+        if n == 2:
+            dic['cat'] = line[: -1]
 
-# compare the sequence between human and mouse
-file_mouse = open('ACE2_mouse.fa')
-output3 = open('mouse_gene','w')
-for line in file_mouse:
-    if not line.startswith('>'):
-        output3.write(line)
-        output3.close()
-file3 = open('mouse_gene')
-seq3 = file3.read()
-# compare the sequence between human and mouse
-edit_distance2 = 0
-for i in range(len(seq1)):
-    if seq1[i]!=seq3[i]:
-       edit_distance2 += 1
-print(edit_distance2)
-# calculate the	percentage	of identical amino acids for each of the three	comparisons
-percentage2 = edit_distance2/len(seq1)*100
-print("%.2f%%" % percentage2)
 
-# compare the sequence between cat and mouse
-edit_distance3 = 0
-for i in range(len(seq2)):
-    if seq2[i] != seq3[i]:
-        edit_distance3 += 1
-print(edit_distance3)
-# calculate the	percentage	of identical amino acids for each of the three	comparisons
-percentage3 = edit_distance3 / len(seq2) * 100
-print("%.2f%%" % percentage3)
+# define a function use blosum62 matrix to get score for each letter and calculate the total score
+# return the score and percentage
+def alignment(seq1, seq2):
+    score = 0
+    edit_distance = 0
+    for i in range(len(seq1)):
+        score += matrix[seq1[i]][seq2[i]]
+        if seq1[i] == seq2[i]:
+            edit_distance += 1
+    percentage = edit_distance / len(seq1)
+    return score, percentage
+# calculation of human and mouse:
+print(alignment(dic['human'],dic['mouse']))
+# human and cat:
+print(alignment(dic['human'],dic['cat']))
+# mouse and cat:
+print(alignment(dic['mouse'],dic['cat']))
+
+# output:
+# alignment score for human and cat:3717.0, the percentage:0.8521739130434782
+# alignment score for human and mouse:3579.0.0, the percentage:8211180124223603%
+# alignment score for cat and mouse：3592.0， the percentage:0.817391304347826%
+
