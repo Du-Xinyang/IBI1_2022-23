966969a9cb39b3539dc270e5a351d20f703ec2dc
Du Xinyang/Nancy <127186904+Du-Xinyang@users.noreply.github.com>
2023-05-23 09:57:29 +0800

    Add some pseudo code

--- Practical9/cds_counter.py
+++ Practical9/cds_counter.py
@@ -1,6 +1,9 @@
 import re
 seq = 'ATGCAATCGACTACGATCTGAGAGGGCCTAA'
+# find the start codon
 sub_seq = str(re.findall(r'^ATG.+', seq))
+# find all stop codons
 num_1 =re.findall(r'TAG|TGA|TAA',sub_seq)
+# since we only consider one start codon at the beginning of the provided sequence, we only need to count the number of the stop codons.
 num = len(num_1)
 print(num)
