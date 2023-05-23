dd84d6493ce7b90542cf1f8e3dbb5564b23e68d2
Du Xinyang/Nancy <127186904+Du-Xinyang@users.noreply.github.com>
2023-05-23 10:41:07 +0800

    upper and lower case in the sequence

--- Practical10/Protein-coding capability calculator.py
+++ Practical10/Protein-coding capability calculator.py
@@ -1,7 +1,8 @@
-# create a function that	determines	whether	a	given	DNA	 sequence	is	likely	to	be	protein-coding	or	not.
+# create a function that determines	whether	a	given DNA sequence	is	likely	to	be	protein-coding	or	not.
 import re
 def g(x):
     x = DNAseq
+# Here we consider the start codon and stop codon are both included in the sequence portion.
     y = re.findall(r'(ATG\S+TGA)',x)
     e = str(y)
     z = len (e)
@@ -13,5 +14,8 @@ def g(x):
         print('non-coding')
     else:
         print('unclear')
-DNAseq = 'ATCTTTAATGCCTTATAACGGAATTGACAGG'
+# assume the input sequence contains both upper and lower case is:
+DNAseq = 'aTCTTtAATGCCTTatAACGGAATTGACaGg'
+DNAseq = DNAseq.upper()
+# show how the function works
 g(DNAseq)
